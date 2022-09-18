0e26604c29b16c6d03fe47af095c28c3cd8e72cf
dandr94 <d.andreev1994@gmail.com>
2022-09-18 22:35:05 +0300

    Added Template and View for About-me section

--- portfolio/portfolio/src/urls.py
+++ portfolio/portfolio/src/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from portfolio.src.views import Home, ShowContacts
+from portfolio.src.views import Home, ShowContacts, AboutDetails
 
 urlpatterns = [
     path('', Home.as_view(), name='index'),
-    path('contacts/', ShowContacts.as_view(), name='contacts')
+    path('contacts/', ShowContacts.as_view(), name='contacts'),
+    path('about_me/', AboutDetails.as_view(), name='about')
 ]
--- portfolio/portfolio/src/views.py
+++ portfolio/portfolio/src/views.py
@@ -1,6 +1,7 @@
-from django.views.generic import TemplateView, ListView
+from django.core.exceptions import ObjectDoesNotExist
+from django.views.generic import TemplateView, ListView, DetailView
 
-from portfolio.src.models import Contacts
+from portfolio.src.models import Contacts, About
 
 
 class Home(TemplateView):
@@ -15,3 +16,17 @@ class ShowContacts(ListView):
     def get_queryset(self):
         queryset = Contacts.objects.all()
         return queryset
+
+
+class AboutDetails(TemplateView):
+    model = About
+    template_name = 'about_me.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        try:
+            context['about'] = About.objects.get()
+        except ObjectDoesNotExist:
+            context['about'] = False
+
+        return context
