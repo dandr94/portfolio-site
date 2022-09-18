3f34992a2daf56bf56748c011477517da3fd15be
dandr94 <d.andreev1994@gmail.com>
2022-09-18 20:56:51 +0300

    Registered About in administration

--- portfolio/portfolio/src/admin.py
+++ portfolio/portfolio/src/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
-from portfolio.src.models import Contacts
+from portfolio.src.models import Contacts, About
 
 
 @admin.register(Contacts)
 class ListOfContacts(admin.ModelAdmin):
     list_display = ['name', 'value']
+
+
+@admin.register(About)
+class AboutInformation(admin.ModelAdmin):
+    list_display = ['full_name', 'job']
