5690553f8a6df7b735d61392b020e8e3ed69dea6
dandr94 <d.andreev1994@gmail.com>
2022-09-18 20:54:40 +0300

    Added About model class

--- portfolio/portfolio/src/models.py
+++ portfolio/portfolio/src/models.py
@@ -23,3 +23,38 @@ class Contacts(models.Model):
 
     def __str__(self):
         return f'{self.name}: {self.value}'
+
+
+class About(models.Model):
+    FIRST_NAME_MAX_LEN = 60
+    JOB_MAX_LEN = 40
+    HOBBIES_MAX_LEN = 100
+    LIKES_MAX_LEN = 100
+
+    full_name = models.CharField(
+        max_length=FIRST_NAME_MAX_LEN,
+        null=False,
+        blank=False
+    )
+
+    job = models.CharField(
+        max_length=JOB_MAX_LEN,
+        null=False,
+        blank=False
+    )
+
+    summary = models.TextField(
+        null=False,
+        blank=False
+    )
+
+    hobbies = models.CharField(
+        max_length=
+        HOBBIES_MAX_LEN,
+        blank=True,
+        null=True)
+
+    likes = models.CharField(
+        max_length=LIKES_MAX_LEN,
+        blank=True,
+        null=True)
