609e28f182da19b85e9f150f287822e2785ca23a
dandr94 <d.andreev1994@gmail.com>
2022-09-23 15:52:02 +0300

    Added js for theme switcher btn

--- portfolio/static/js/main.js
+++ portfolio/static/js/main.js
@@ -1,19 +1,46 @@
-const setTheme = theme => document.documentElement.className = theme;
-
-
 const coll = document.getElementsByClassName("collapsible");
 
 for (let i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    const content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
-  });
+    coll[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        const content = this.nextElementSibling;
+        if (content.style.display === "block") {
+            content.style.display = "none";
+        } else {
+            content.style.display = "block";
+        }
+    });
 }
+
+let btnText = document.getElementById('switch')
+
+
+function setTheme(themeName) {
+    localStorage.setItem('theme', themeName);
+    document.documentElement.className = themeName;
+}
+
+function toggleTheme() {
+    if (localStorage.getItem('theme') === 'dark-theme') {
+        setTheme('light-theme');
+        btnText.textContent = 'Dark'
+    } else {
+        setTheme('dark-theme');
+        btnText.textContent = 'Light'
+    }
+}
+
+(function () {
+    if (localStorage.getItem('theme') === 'dark-theme') {
+        setTheme('dark-theme');
+        btnText.textContent = 'Light'
+
+    } else {
+        setTheme('light-theme');
+        btnText.textContent = 'Dark'
+
+    }
+})();
 
 
 
